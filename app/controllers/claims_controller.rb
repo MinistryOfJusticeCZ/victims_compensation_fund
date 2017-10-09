1399d918ed62e93e0581c326934f71ddec19ceac
Ondřej Ezr <oezr@msp.justice.cz>
2017-10-09 16:44:58 +0200

    deploy to lab

--- app/controllers/claims_controller.rb
+++ app/controllers/claims_controller.rb
@@ -16,7 +16,7 @@ class ClaimsController < ApplicationController
   def show
     @payments_schema = PaymentSchema.new(columns: ['offender-fullname', 'payment_uid', 'value'], outputs: ['grid'])
     @payments_schema.add_filter('claim_id', '=', @claim.id)
-    @appeals_schema = AppealSchema.new(columns: ['victim-fullname'], outputs: ['grid'])
+    @appeals_schema = AppealSchema.new(columns: ['file_uid', 'victim-fullname', 'payment_type'], outputs: ['grid'])
     @appeals_schema.add_filter('claim_id', '=', @claim.id)
   end
 
