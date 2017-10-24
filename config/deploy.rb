5a5f94508911ce42a56d6811c42846c57910f2b1
Ondřej Ezr <oezr@msp.justice.cz>
2017-10-25 00:35:12 +0200

    bump capistrano version

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid only for current version of Capistrano
-lock "3.9.1"
+lock "3.10.0"
 
 set :application, "victims_compensation_fund"
 set :repo_url, "git@git.servis.justice.cz:internal_apps/victims_compensation_fund.git"
