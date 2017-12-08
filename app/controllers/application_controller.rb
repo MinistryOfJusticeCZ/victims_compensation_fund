9df88e7e5b53cd8f41b656e754857aa80f4282a6
Ondřej Ezr <oezr@msp.justice.cz>
2017-12-08 18:29:12 +0100

    require login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  def require_login?
+    true
+  end
+
   def internal_network?
     if Rails.env.production?
       super
