cb0d0065ad0335e81df98da4b155344eb2d49070
Ondřej Ezr <oezr@msp.justice.cz>
2018-01-30 16:53:12 +0100

    working js translations in grid(egov_utils)

--- app/schemas/redemption_schema.rb
+++ app/schemas/redemption_schema.rb
@@ -3,7 +3,7 @@ class RedemptionSchema < AzaharaSchema::ModelSchema
   filter_operators 'debt-offender-person-fullname', ['~']
 
   def default_columns
-    cols = ['debt-offender-person-fullname', 'payment-value', 'payment-currency_code']
+    cols = ['debt-claim-file_uid', 'debt-offender-person-fullname', 'payment-value', 'payment-currency_code', 'payment-payment_uid']
     cols << 'author-fullname' if EgovUtils::User.current.has_role?('compensator')
     cols
   end
