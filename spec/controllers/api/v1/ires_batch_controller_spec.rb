50bdf2574cc698f2139578081310e9c1fa0c1be4
Ondřej Ezr <oezr@msp.justice.cz>
2018-02-09 16:58:13 +0100

    add pending example for non available payment

--- spec/controllers/api/v1/ires_batch_controller_spec.rb
+++ spec/controllers/api/v1/ires_batch_controller_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe Api::V1::IresBatchController, type: :controller do
 
   describe '#create' do
     let(:document){ doc = File.open('spec/libs/ires/data/prijmiInformaceOZaplaceni.xml') { |f| Nokogiri::XML(f) } }
+    let(:payment){ FactoryBot.create(:payment, uuid: '788d5c2a-43f1-43f5-95fe-b158b69d9f8c', status: 'sent') }
     let(:encoded_message) { Base64.encode64 document.to_xml }
 
     before(:each) do
@@ -15,8 +16,15 @@ RSpec.describe Api::V1::IresBatchController, type: :controller do
     end
 
     it 'sends response' do
+      payment;
       post :create, params: { xml_data: encoded_message }
       expect( JSON.parse(response.body).keys ).to include('return')
+    end
+
+
+    pending 'should fail gracefully - without existing payment' do
+      post :create, params: { xml_data: encoded_message }
+      expect( JSON.parse(response.body).keys ).to include('error')
     end
 
   end
--- spec/factories/payments.rb
+++ spec/factories/payments.rb
@@ -1,7 +1,9 @@
 FactoryBot.define do
   factory :payment do
-    status 1
     value 1.5
     currency_code 'czk'
+    status nil
+    sequence(:payment_uid){|n| "88#{n.to_s.rjust(6, "0")}#{Time.now.strftime("%y")}"}
+    uuid nil
   end
 end
