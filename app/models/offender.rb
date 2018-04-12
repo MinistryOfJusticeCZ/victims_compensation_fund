742585342ca8dc53e9048542ff80fce631911801
Ondřej Ezr <oezr@msp.justice.cz>
2018-04-12 14:00:15 +0200

    fix offender bug

--- app/models/offender.rb
+++ app/models/offender.rb
@@ -9,7 +9,7 @@ class Offender < ApplicationRecord
   def to_s
     case person.person_type
     when 'natural'
-      "#{person.fullname} (#{I18n.t(:text_born_on_at, place: person.birth_place, date: I18n.l(person.birth_date.to_date))})"
+      "#{person.fullname} (#{I18n.t(:text_born_on_at, place: person.natural_person.birth_place, date: I18n.l(person.natural_person.birth_date.to_date))})"
     else
       person.to_s
     end
