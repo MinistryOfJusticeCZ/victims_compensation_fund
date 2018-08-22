8c5cb4a11bf60049aa3bbc5d47b9636a575d7b3d
Ondřej Ezr <oezr@msp.justice.cz>
2018-08-22 18:18:53 +0200

    we dont need the organization_code actually

--- app/jobs/send_payment_job.rb
+++ app/jobs/send_payment_job.rb
@@ -6,7 +6,7 @@ class SendPaymentJob < ApplicationJob
   def perform(payment_id, organization_code=nil)
     sender = Ires::Sender.new
     payment = Payment.with_deleted.find(payment_id)
-    organization_code ||= payment.claim.court_uid
-    sender.send_payment_prescription!(payment, organization_code, job_id)
+    # organization_code ||= payment.claim.court_uid
+    sender.send_payment_prescription!(payment, job_id)
   end
 end
