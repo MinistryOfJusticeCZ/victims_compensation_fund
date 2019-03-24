b67fabc6e7d5001e4732a1b607419c1410d639e9
Ondřej Ezr <oezr@msp.justice.cz>
2019-03-24 16:30:12 +0100

    satisfaction transfers label and tests

--- app/helpers/satisfactions_helper.rb
+++ app/helpers/satisfactions_helper.rb
@@ -1,12 +1,23 @@
 module SatisfactionsHelper
-
-  def satisfaction_transfer_redemption_path(appeal, redemption)
-    value = if appeal.unsatisfied_amount && appeal.unsatisfied_amount < redemption.payment.value
-              appeal.unsatisfied_amount
-            else
-              value ||= redemption.payment.value
-            end
-    satisfactions_path(satisfaction: {appeal_id: appeal.id, fund_transfers_attributes: {'0' => {redemption_id: redemption.id, value: value}}})
+  def satisfaction_transfer_amount(appeal, redemption)
+    if appeal.unsatisfied_amount && appeal.unsatisfied_amount < redemption.payment.value
+      appeal.unsatisfied_amount
+    else
+      value ||= redemption.payment.value
+    end
   end
 
+  def buton_to_satisfaction_transfer_redemption(appeal, redemption)
+    amount = satisfaction_transfer_amount(appeal, redemption)
+    button_to(
+      t('label_transfer_amount_to_victim', victim: appeal.victim.fullname, amount: amount),
+      satisfaction_transfer_redemption_path(appeal, redemption, amount),
+      class: 'btn btn-secondary'
+    )
+  end
+
+  def satisfaction_transfer_redemption_path(appeal, redemption, value = nil)
+    value ||= satisfaction_transfer_amount(appeal, redemption)
+    satisfactions_path(satisfaction: {appeal_id: appeal.id, fund_transfers_attributes: {'0' => {redemption_id: redemption.id, value: value}}})
+  end
 end
--- spec/helpers/satisfactions_helper_spec.rb
+++ spec/helpers/satisfactions_helper_spec.rb
@@ -3,7 +3,22 @@ require 'rails_helper'
 RSpec.describe SatisfactionsHelper, type: :helper do
 
   let(:redemption){ double(Redemption, id: 2, payment: double(value: 200)) }
-  let(:appeal){ double(Appeal, id: 1) }
+  let(:appeal){ double(Appeal, id: 1, victim: double(fullname: 'Poskozeny')) }
+
+  describe '#buton_to_satisfaction_transfer_redemption' do
+    it 'transfers maximaly appeal value' do
+      allow(appeal).to receive('unsatisfied_amount').and_return(10)
+
+      expect(helper).to receive(:t)
+                          .with('label_transfer_amount_to_victim', victim: 'Poskozeny', amount: 10)
+                          .and_return('Zaplatit 10 Poskozeny')
+      expect(helper).to receive('satisfaction_transfer_redemption_path')
+                          .with(appeal, redemption, 10)
+                          .and_return('/path/to/transfer/10/to/1')
+      expect(helper).to receive(:button_to).with('Zaplatit 10 Poskozeny', '/path/to/transfer/10/to/1', have_key(:class))
+      helper.buton_to_satisfaction_transfer_redemption(appeal, redemption)
+    end
+  end
 
   describe "#satisfaction_transfer_redemption_path" do
     it "gets redemption value if appeal value is nil" do
